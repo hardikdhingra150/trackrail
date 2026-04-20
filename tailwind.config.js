7fa3a93ce6bfe8dc888612ce8d27b4a9f1be5b63
hardikdhingra150 <hardikdhingra150@gmail.com>
2026-04-20 23:23:55 +0530

    file added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navy: {
+          DEFAULT: "#07111c",
+          soft: "#0d1b2a",
+          2: "#102235",
+          card: "#13283a",
+        },
+      },
+      fontFamily: {
+        sans: ["Manrope", "sans-serif"],
+        display: ["'Playfair Display'", "serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
